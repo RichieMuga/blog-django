f3c6ad838a022c8d873e951fe65c787ce77b8d01
Richie Mugambi <richiemugambi4@gmail.com>
2024-01-02 16:24:13 +0300

    feat: changed folder structure

--- blogsPractise/models.py
+++ blogsPractise/models.py
@@ -32,3 +32,13 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.headline
+
+
+class Person(models.Model):
+    SHIRT_SIZES = {
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+    }
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
